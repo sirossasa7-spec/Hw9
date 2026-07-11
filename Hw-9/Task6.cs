7b238ce0f2bcfde4739d67052c20dc74aa01d5e4
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-07-11 22:42:45 +0200

    Task6

---
+++ Hw-9/Task6.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Hw_9
+{
+    internal class Task6
+    {
+        static List<T> FilterByTwoCriteria<T>(List<T> list, Predicate<T> p1, Predicate<T> p2)
+        {
+            List<T> result = new List<T>();
+
+            foreach (T item in list)
+            {
+                if (p1(item) && p2(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            List<int> numbers = new List<int>()
+            {
+                2,4,5,8,10,15
+            };
+
+            List<int> result = FilterByTwoCriteria(
+                numbers,
+                x => x > 5,
+                x => x % 2 == 0);
+
+            foreach (int item in result)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            List<string> words = new List<string>()
+            {
+                "apple",
+                "cat",
+                "banana",
+                "dog"
+            };
+
+            List<string> result2 = FilterByTwoCriteria(
+                words,
+                x => x.Length > 3,
+                x => x.Contains("a"));
+
+            foreach (string item in result2)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
