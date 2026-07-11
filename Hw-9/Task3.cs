5a7b1febd28c8803c8ef0a5f5252128fb3097bf5
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-07-11 22:27:09 +0200

    Task3

---
+++ Hw-9/Task3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Hw_9
+{
+    internal class Task3
+    {
+        static T Sum<T>(T[] array)
+        {
+            dynamic sum = 0;
+
+            foreach (T item in array)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(Sum(a));
+        }
+    }
+}
