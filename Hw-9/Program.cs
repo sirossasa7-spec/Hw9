008d7bb9c43adf266a4cdee08b64b85b0c620740
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-07-11 22:11:07 +0200

    Add project files.

---
+++ Hw-9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Hw_9
+{
+    internal class Program
+    {
+        static T Max<T>(T a, T b, T c) where T : IComparable<T>
+        {
+            T max = a;
+
+            if (b.CompareTo(max) > 0)
+                max = b;
+
+            if (c.CompareTo(max) > 0)
+                max = c;
+
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Max(5, 8, 3));
+            Console.WriteLine(Max(2.5, 7.8, 1.2));
+        }
+    }
+}
